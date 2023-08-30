af7e6d56031b80b653dc857ccc4016e4ff536344
Pedare <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>
2023-08-31 09:26:00 +0930

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    gender = db.Column(db.String(10), nullable=False)
+    interests = db.Column(db.String(255), nullable=False)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/registration", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        name = request.form.get("name")
+        gender = request.form.get("gender")
+        interests = request.form.get("interests")
+
+        new_user = User(name=name, gender=gender, interests=interests)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return render_template("confirmation.html")
+
+    return render_template("registration.html")
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()  # Create database tables
+    app.run(debug=True)
